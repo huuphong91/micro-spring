82b6d5797b03936544ef2b163110396c25e4e6b3
toan <nguyenhuutoandn@gmail.com>
2024-06-21 04:11:58 +0000

    init project

--- food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/exception/handler/OrderGlobalExceptionHandler.java
+++
@@ -1,4 +0,0 @@
-package com.food.ordering.system.order.service.application.exception.handler;
-
-public class OrderGlobalExceptionHandler {
-}
--- food-ordering-system/order-service/order-application/src/main/java/com/food/ordering/system/order/service/application/rest/OrderCOntroller.java
+++
@@ -1,4 +0,0 @@
-package com.food.ordering.system.order.service.application.rest;
-
-public class OrderCOntroller {
-}
